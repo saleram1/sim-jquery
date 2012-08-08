343a7d5235a6b84d18df9e496e6e85ece31c5d17
Michael J Salera <mikesalera@mac.com>
2012-08-08 05:59:07 -0400

    Added Domain obj for pre-send validation errors such as negative price or quantity

---
+++ grails-app/domain/com/mercadolibre/apps/sim/ItemValidationError.groovy
@@ -0,0 +1,8 @@
+package com.mercadolibre.apps.sim
+
+/** 
+ *  Example {"message":"access_token.invalid","error":"Invalid OAuth access token.","status":403,"cause":[]}
+ */
+class ItemValidationError extends ApiError {
+	
+}
