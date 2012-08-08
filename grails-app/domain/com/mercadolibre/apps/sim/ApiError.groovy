708d54184482a63d4a29e1a87cd93e3f30c62d84
Michael J Salera <mikesalera@mac.com>
2012-08-08 06:01:19 -0400

    Added to Domain obj abillity to have fileSource.originalFilename and aRowNumber so one lonely Seller might correct the error and re-submit

--- grails-app/domain/com/mercadolibre/apps/sim/ApiError.groovy
+++ grails-app/domain/com/mercadolibre/apps/sim/ApiError.groovy
@@ -7,15 +7,22 @@ import groovy.transform.ToString
  */
 @ToString
 class ApiError {
+	String originalFilename
 	Integer status = 400		// Bad request
 	String error
 	String message
 	String cause
 		
 	static constraints = {
-		status(min: 400, max: 499)
+		cause(nullable: true, blank: false)
 		error(nullable: false, blank: false)
 		message(nullable: false, blank: false)
-		cause(nullable: true, blank: false)
+		originalFilename(nullable: true, blank: false)
+		//rowNumber(min:1, max:65536)
+		status(min: 400, max: 499)
 	}
+	
+	static mapping = {
+        tablePerHierarchy false
+    }	
 }
