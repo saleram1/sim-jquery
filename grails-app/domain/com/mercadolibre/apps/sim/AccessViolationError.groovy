322605158c192d10d512f95860965116336e1cd7
Michael J Salera <mikesalera@mac.com>
2012-08-08 06:00:12 -0400

    Added Domain obj for general 403 'Forbidden' error cases - which means the App might not be authorised for the User might not be able to Sell / Vender

--- grails-app/domain/com/mercadolibre/apps/sim/AccessViolationError.groovy
+++ grails-app/domain/com/mercadolibre/apps/sim/AccessViolationError.groovy
@@ -4,15 +4,17 @@ package com.mercadolibre.apps.sim
  *  Example {"message":"access_token.invalid","error":"Invalid OAuth access token.","status":403,"cause":[]}
  */
 class AccessViolationError extends ApiError {
+	
 	//
 	// Some User objects are not setup yet to POST items - there is no error message but 
-	// we re-use the one from the 'bad access token' one
+	// we re-use the one from  'bad access token' 
 	// 
-	AccessViolationError() {
+	AccessViolationError(String anOriginalFilename, Integer aRowNumber = 1) {
 		super()
 		
 		status = 403
 		error  = "access_token.invalid"
 		message = "Invalid OAuth access token."
+		originalFilename = anOriginalFilename
 	}
 }
