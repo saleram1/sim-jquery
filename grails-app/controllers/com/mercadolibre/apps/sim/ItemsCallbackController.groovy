35466ec9aebba1020429327837be14101ccc92fb
Michael J Salera <mikesalera@mac.com>
2012-08-08 18:00:22 -0700

    Added notifications callback for Items feed - updating appl entry tomorrow

---
+++ grails-app/controllers/com/mercadolibre/apps/sim/ItemsCallbackController.groovy
@@ -0,0 +1,19 @@
+package com.mercadolibre.apps.sim
+
+import grails.converters.JSON
+
+
+class ItemsCallbackController {
+
+//	def itemUpdaterService
+
+    def index() { 
+		log.info request.JSON
+//		
+//		itemUpdaterService.consumeMessage(request.JSON))
+//	
+		def responseMap = ['message': "Items message processed", 'error': "none", 'status': 200, 'cause': []]
+		response.status = 200
+		render(responseMap as JSON)
+	}
+}
---
+++ test/unit/com/mercadolibre/apps/sim/ItemsCallbackControllerTests.groovy
@@ -0,0 +1,17 @@
+package com.mercadolibre.apps.sim
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ItemsCallbackController)
+class ItemsCallbackControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
