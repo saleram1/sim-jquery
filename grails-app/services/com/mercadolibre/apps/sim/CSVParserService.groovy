aceb2e87190be37ae2c1c0b4e1284e4df1827b5e
Michael J Salera <mikesalera@mac.com>
2012-09-05 11:46:30 -0700

    Introduce robust CSV to map parsing courtesy OpenCSV

--- grails-app/services/com/mercadolibre/apps/sim/CSVParserService.groovy
+++ grails-app/services/com/mercadolibre/apps/sim/CSVParserService.groovy
@@ -1,8 +1,34 @@
 package com.mercadolibre.apps.sim
 
-class CSVParserService {
+import org.grails.plugins.csv.CSVMapReader
 
-    def serviceMethod() {
 
+class CsvParserService {
+
+  static transactional = false
+
+  /**
+   * Make this more robust by using the OpenCSV (latest) library which handles descriptions with embedded quotes and
+   * so forth
+   *
+   * @return List<Map>  one per row
+   */
+  List parseItemInventoryFile(File inputFile) {
+    def rowList = []
+
+    // create Reader if the file contents are *more than just the header* line
+    if (inputFile?.exists() && inputFile.length() > MIN_LENGTH) {
+      FileReader reader = new FileReader(inputFile)
+
+      //assumes the first line of the file has the field names
+      new CSVMapReader(reader).each { map ->
+        rowList.add(map)
+      }
+
+      reader.close()
     }
+    return rowList
+  }
+
+  static final Integer MIN_LENGTH = 142
 }
--- test/integration/com/mercadolibre/apps/sim/CSVParserServiceTests.groovy
+++ test/integration/com/mercadolibre/apps/sim/CSVParserServiceTests.groovy
@@ -1,17 +1,54 @@
 package com.mercadolibre.apps.sim
 
-
-
-import grails.test.mixin.*
 import org.junit.*
+import org.springframework.beans.BeansException
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
+import grails.test.mixin.*
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
-@TestFor(CSVParserService)
-class CSVParserServiceTests {
+@TestFor(CsvParserService)
+class CsvParserServiceTests implements ApplicationContextAware {
 
-    void testSomething() {
-        fail "Implement me"
+  ApplicationContext applicationContext
+  CsvParserService csvParserService
+
+  @Before
+  void setUp() {
+    // Setup logic here
+  }
+
+
+  void testCsvParseStrippedFromBootStrap() {
+    assert true
+    /*
+        // appropriate for the BootStrap
+        ApplicationContext applicationContext = servletContext.getAttribute(GrailsApplicationAttributes.APPLICATION_CONTEXT)
+        CsvParserService csvParser = (CsvParserService) applicationContext.getBean("csvParserService")
+        println csvParser
+      */
+  }
+
+  void testSystemUnderTestGetsInjected() {
+    assert applicationContext
+    assert csvParserService
+  }
+
+  void testAllSamplesCanBeParsedToMaps() {
+    File csvDirectory = applicationContext.getResource(File.separator + "WEB-INF" + File.separator + "docs").getFile();
+    csvDirectory.list().each { csvFile ->
+      if (csvFile.endsWith(".csv")) {
+        def myFile = new File("${csvDirectory.path}/${csvFile}")
+        println myFile.path
+        assert myFile.exists()
+        assert (csvParserService.parseItemInventoryFile(myFile).size() > 0)
+      }
     }
+  }
+
+  void setApplicationContext(ApplicationContext aContext) throws BeansException {
+    this.applicationContext = aContext
+  }
 }
