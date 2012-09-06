22355cbe665fca14faf2c3696d0876c3feb1b9fe
Michael J Salera <mikesalera@mac.com>
2012-09-06 11:24:54 -0700

    Proper error reporting for 400 range errors

--- grails-app/services/com/mercadolibre/apps/sim/ImportService.groovy
+++ grails-app/services/com/mercadolibre/apps/sim/ImportService.groovy
@@ -3,13 +3,14 @@ package com.mercadolibre.apps.sim
 import com.mercadolibre.apps.sim.util.CSVImporter
 
 import groovyx.net.http.ContentType
-import static groovyx.net.http.ContentType.JSON
+
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.ParserRegistry
 import org.apache.http.entity.ContentProducer
 import org.apache.http.entity.EntityTemplate
 import org.apache.http.message.BasicHeader
 import grails.converters.JSON
+import com.mercadolibre.apps.sim.data.bo.core.VanillaItemListing
 
 /**
  *
@@ -44,7 +45,7 @@ class ImportService {
             // @TODO support a 'required attribute' message in the output of ItemImport - such as options for Fashion
 
             // if user has not already uploaded and the Category is okay
-            if (categoryService.isValidCategory(aProperItem.category) && !categoryService.isFunkyFashionFootwearCategory(aProperItem.category)) {
+            if (categoryService.isValidCategory(aProperItem.category_id) && !categoryService.isFunkyFashionFootwearCategory(aProperItem.category_id)) {
               String newItemId = pushItemToMarketplace(itemImport, fileSource, idx, aProperItem, accessToken)
               aProperItem.mercadoLibreItemId = newItemId
 
@@ -54,7 +55,7 @@ class ImportService {
               }
             }
             else {
-              log.warn "Cannot support category id: ${aProperItem.category} in the current version"
+              log.warn "Cannot support category_id id: ${aProperItem.category_id} in the current version"
             }
           }
           catch (Throwable tr) {
@@ -110,14 +111,16 @@ class ImportService {
         newItemId = json['id']
       }
       response.failure = { resp, json ->
-        assert json.size() == 4
+        //assert json.size() == 4
         def anException = new ApiError(status: json['status'], error: json['error'], message: json['message'], originalFilename: fileSource.originalFilename)
+        log.error anException
         anException.save()
         itemImport.addToErrs(anException)
       }
       response.'403' = { resp, json ->
-        assert json.size() == 4
+        //assert json.size() == 4
         def anError = new AccessViolationError(fileSource.originalFilename)
+        log.error anError
         anError.save()
         itemImport.addToErrs(anError)
       }
