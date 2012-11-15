b6ab2d92d3dfae3cb41b8b005de78f03b5b9f582
Michael J Salera <mikesalera@mac.com>
2012-11-15 15:05:03 -0800

    Added the central authService(com.mercadolibre.apps.auth.OAuthService)

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
 // Place your Spring DSL code here
 beans = {
+	authService(com.mercadolibre.apps.auth.OAuthService)
+	
+	'VerifyController'(VerifyController) { bean ->
+	  bean.autowire = 'byName'
+	  authService = ref(authService)
+	}
 }
