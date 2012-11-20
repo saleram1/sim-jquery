d055d7d52cee22666ec91046b4b8a7f06139fe2b
Johanan Lancaon <johanan.lancaon@mercadolibre.com>
2012-11-20 13:00:52 -0800

    Added FQN.

--- grails-app/domain/com/mercadolibre/apps/sim/data/bo/imports/meli/Category.groovy
+++ grails-app/domain/com/mercadolibre/apps/sim/data/bo/imports/meli/Category.groovy
@@ -4,10 +4,12 @@ class Category {
   
   String name
   String meliId
+  String fqn
     
   static constraints = {
      name(nullable: false)
      meliId(nullable: false)
+     fqn(nullable: false)
   }
   
   String toString() {
