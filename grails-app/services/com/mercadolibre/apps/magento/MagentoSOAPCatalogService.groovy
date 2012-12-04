5a57bc0508a520acf0fa7cdbe7a445cb575d3628
Michael J Salera <mikesalera@mac.com>
2012-12-04 07:49:12 -0800

    Now all parent and child SKUs can be returned so  have to remove logging and ensure the skuList is valid

--- grails-app/services/com/mercadolibre/apps/magento/MagentoSOAPCatalogService.groovy
+++ grails-app/services/com/mercadolibre/apps/magento/MagentoSOAPCatalogService.groovy
@@ -48,8 +48,14 @@ class MagentoSOAPCatalogService extends MagentoSOAPBase {
     }
   }
 
-
+  /**
+   *
+   * @param cap
+   * @return List < String >  sku or productId
+   */
   List getProductsAssignedToCategory(CatalogCategoryAssignedProductsRequestParam cap) {
+    def skuList = []
+
     CatalogCategoryAssignedProductsResponseParam responseParam =
       mageProxy.getMageApiModelServerWsiHandlerPort().catalogCategoryAssignedProducts(cap)
 
@@ -57,13 +63,18 @@ class MagentoSOAPCatalogService extends MagentoSOAPBase {
     responseParam.result?.complexObjectArray?.each() { CatalogAssignedProduct product ->
       println "Product? type=${product.type}  Entity ID ${product.productId}   sku = ${product.sku}"
 
+      skuList << "${product.sku}"
+
       def related = getRelatedProductsForProductId(cap.sessionId, product.sku)
       related?.each { CatalogProductEntity prod ->
         println "    -->>  Child Product? type=${prod.type}  Quantity: ${prod.name}  sku = ${prod.sku} size = ${prod.websiteIds.toString()} "
+        if (!skuList.contains("${prod.sku}")) {
+          skuList << "${prod.sku}"
+        }
       }
     }
-
-    return responseParam.result.complexObjectArray.toList()
+    skuList
+    //  return responseParam.result.complexObjectArray.toList()
   }
 
   /**
@@ -97,17 +108,8 @@ class MagentoSOAPCatalogService extends MagentoSOAPBase {
 
 
   /**
-   *
-   * @param sessionId
-   * @param sku
-   * @return
-   */
-  Map getSimpleProductDetailsForProductId(String sessionId, String sku) {
-    return [:]
-  }
-  
-  /**
    * Critical call to retrieve actual SIMPLE Saleable product id's
+   * skuList << "${product.sku}"
    *
    * @param sessionId
    * @param productId
@@ -115,65 +117,65 @@ class MagentoSOAPCatalogService extends MagentoSOAPBase {
    */
   Map getProductDetails(String sessionId, String productId) {
 
-	def productDetailMap = [:]
-	  	
-	def cpip = new CatalogProductInfoRequestParam()
-	cpip.productId = productId
-	cpip.sessionId = sessionId
-	cpip.store = ""
-	
-	CatalogProductReturnEntity catalogProductReturnEntity = mageProxy.getMageApiModelServerWsiHandlerPort().catalogProductInfo(cpip).result
-	
-	ArrayOfString categoryArray = catalogProductReturnEntity.categories
-	def categories = categoryArray.complexObjectArray
-	
-	ArrayOfString websiteArray = catalogProductReturnEntity.websites
-	def websites = websiteArray.complexObjectArray
-		
-	ArrayOfString categoryIdArray = catalogProductReturnEntity.categoryIds
-	def categoryIds = categoryIdArray.complexObjectArray
-	
-	
-	ArrayOfString webSiteIdsArray = catalogProductReturnEntity.websiteIds
-	def websiteIds = webSiteIdsArray.complexObjectArray
-	
-	productDetailMap = ["productId":catalogProductReturnEntity.productId,
-					"sku":catalogProductReturnEntity.sku,
-					"set":catalogProductReturnEntity.set,
-					"type":catalogProductReturnEntity.type,
-					"categories":categories,
-					"websites":websites,
-					"createdAt":catalogProductReturnEntity.createdAt,
-					"updatedAt":catalogProductReturnEntity.updatedAt,
-					"typeId":catalogProductReturnEntity.typeId,
-					"type":	catalogProductReturnEntity.type,
-					"name":catalogProductReturnEntity.name,
-					"description":catalogProductReturnEntity.description,
-					"shortDescription":catalogProductReturnEntity.shortDescription,
-					"weight":catalogProductReturnEntity.weight,
-					"status":catalogProductReturnEntity.status,
-					"urlKey":catalogProductReturnEntity.urlKey,
-					"urlPath":catalogProductReturnEntity.urlPath,
-					"visibility":catalogProductReturnEntity.visibility,
-					"categoryIds":categoryIds,
-					"websiteIds":websiteIds,
-					"hasOptions":catalogProductReturnEntity.hasOptions,
-					"giftMessageAvailable":catalogProductReturnEntity.giftMessageAvailable,
-					"price":catalogProductReturnEntity.price,
-					"specialPrice":catalogProductReturnEntity.specialPrice,
-					"specialFromDate":catalogProductReturnEntity.specialFromDate,
-					"specialToDate":catalogProductReturnEntity.specialToDate,
-					"taxClassId":catalogProductReturnEntity.taxClassId,
-					"tierPrice":catalogProductReturnEntity.tierPrice,
-					"metaTitle":catalogProductReturnEntity.metaTitle,
-					"metaKeyword":catalogProductReturnEntity.metaKeyword,
-					"metaDescription":catalogProductReturnEntity.metaDescription,
-					"customDesign":catalogProductReturnEntity.customDesign,
-					"customLayoutUpdate":catalogProductReturnEntity.customLayoutUpdate,
-					"optionsContainer":catalogProductReturnEntity.optionsContainer,
-					"additionalAttributes":catalogProductReturnEntity.additionalAttributes,
-					"enableGoogleCheckout":catalogProductReturnEntity.enableGooglecheckout
-  					]
-  	productDetailMap
+    def productDetailMap = [:]
+
+    def cpip = new CatalogProductInfoRequestParam()
+    cpip.productId = productId
+    cpip.sessionId = sessionId
+    cpip.store = ""
+
+    CatalogProductReturnEntity catalogProductReturnEntity = mageProxy.getMageApiModelServerWsiHandlerPort().catalogProductInfo(cpip).result
+
+    ArrayOfString categoryArray = catalogProductReturnEntity.categories
+    def categories = categoryArray.complexObjectArray
+
+    ArrayOfString websiteArray = catalogProductReturnEntity.websites
+    def websites = websiteArray.complexObjectArray
+
+    ArrayOfString categoryIdArray = catalogProductReturnEntity.categoryIds
+    def categoryIds = categoryIdArray.complexObjectArray
+
+    ArrayOfString webSiteIdsArray = catalogProductReturnEntity.websiteIds
+    def websiteIds = webSiteIdsArray.complexObjectArray
+
+
+    productDetailMap = ["productId": catalogProductReturnEntity.productId,
+        "sku": catalogProductReturnEntity.sku,
+        "set": catalogProductReturnEntity.set,
+        "type": catalogProductReturnEntity.type,
+        "categories": categories,
+        "websites": websites,
+        "createdAt": catalogProductReturnEntity.createdAt,
+        "updatedAt": catalogProductReturnEntity.updatedAt,
+        "typeId": catalogProductReturnEntity.typeId,
+        "type": catalogProductReturnEntity.type,
+        "name": catalogProductReturnEntity.name,
+        "description": catalogProductReturnEntity.description,
+        "shortDescription": catalogProductReturnEntity.shortDescription,
+        "weight": catalogProductReturnEntity.weight,
+        "status": catalogProductReturnEntity.status,
+        "urlKey": catalogProductReturnEntity.urlKey,
+        "urlPath": catalogProductReturnEntity.urlPath,
+        "visibility": catalogProductReturnEntity.visibility,
+        "categoryIds": categoryIds,
+        "websiteIds": websiteIds,
+        "hasOptions": catalogProductReturnEntity.hasOptions,
+        "giftMessageAvailable": catalogProductReturnEntity.giftMessageAvailable,
+        "price": catalogProductReturnEntity.price,
+        "specialPrice": catalogProductReturnEntity.specialPrice,
+        "specialFromDate": catalogProductReturnEntity.specialFromDate,
+        "specialToDate": catalogProductReturnEntity.specialToDate,
+        "taxClassId": catalogProductReturnEntity.taxClassId,
+        "tierPrice": catalogProductReturnEntity.tierPrice,
+        "metaTitle": catalogProductReturnEntity.metaTitle,
+        "metaKeyword": catalogProductReturnEntity.metaKeyword,
+        "metaDescription": catalogProductReturnEntity.metaDescription,
+        "customDesign": catalogProductReturnEntity.customDesign,
+        "customLayoutUpdate": catalogProductReturnEntity.customLayoutUpdate,
+        "optionsContainer": catalogProductReturnEntity.optionsContainer,
+        "additionalAttributes": catalogProductReturnEntity.additionalAttributes,
+        "enableGoogleCheckout": catalogProductReturnEntity.enableGooglecheckout
+    ]
+    productDetailMap
   }
 }
