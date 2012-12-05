54bd2eac35954f212de942ff794774d2289cfb3c
Johanan Lancaon <johanan.lancaon@mercadolibre.com>
2012-12-05 14:24:38 -0800

    adding more methods for getting hte various custom product options

--- grails-app/services/com/mercadolibre/apps/magento/MagentoSOAPProductOptionsService.groovy
+++ grails-app/services/com/mercadolibre/apps/magento/MagentoSOAPProductOptionsService.groovy
@@ -1,8 +1,14 @@
 package com.mercadolibre.apps.magento
 
+import magento.CatalogAttributeOptionEntityArray
 import magento.CatalogProductAttributeOptionsRequestParam
 import magento.CatalogProductAttributeOptionsResponseParam
-import magento.CatalogAttributeOptionEntityArray
+import magento.CatalogProductCustomOptionListArray
+import magento.CatalogProductCustomOptionListRequestParam
+import magento.CatalogProductCustomOptionListResponseParam
+import magento.CatalogProductCustomOptionTypesArray
+import magento.CatalogProductCustomOptionTypesRequestParam
+import magento.CatalogProductCustomOptionTypesResponseParam
 
 /**
  * Created with IntelliJ IDEA.
@@ -45,4 +51,94 @@ class MagentoSOAPProductOptionsService extends MagentoSOAPBase {
     productOptionList
   }
 
+  /**
+   * Get a list of product options types based on sessionId
+   *
+   * @param sessionId
+   * @return List of Product Custom Option Types (expando object containing the various attributes)
+   */ 
+  List getProductCustomOptionTypes(String sessionId) {
+	  
+	def customProductOptionTypeList = []
+  
+	CatalogProductCustomOptionTypesRequestParam cpcotp = new CatalogProductCustomOptionTypesRequestParam()
+	cpcotp.sessionId = sessionId
+	  
+	CatalogProductCustomOptionTypesResponseParam catalogProductCustomOptionTypesResponseParam = mageProxy.getMageApiModelServerWsiHandlerPort().catalogProductCustomOptionTypes(cpcotp)
+	
+	CatalogProductCustomOptionTypesArray catalogProductCustomOptionTypesArray = catalogProductCustomOptionTypesResponseParam.result
+	  
+	catalogProductCustomOptionTypesArray.complexObjectArray.each {
+	  def productCustomOptionExpando = new Expando()
+	  productCustomOptionExpando.label = it.label
+	  productCustomOptionExpando.value = it.value
+	  
+	  customProductOptionTypeList.add(productCustomOptionExpando)
+	}
+	customProductOptionTypeList
+  }
+  
+  /**
+   * Get a list of product options list based on sessionId
+   *
+   * @param sessionId
+   * @param productId
+   * @return List of Product Custom Option List (expando object containing the various attributes)
+   */
+  List getProductCustomOptionList(String sessionId, String productId) {
+	  
+	def customProductOptionList = []
+  
+	CatalogProductCustomOptionListRequestParam cpcolp = new CatalogProductCustomOptionListRequestParam()
+	cpcolp.sessionId = sessionId
+	cpcolp.productId = productId
+	cpcolp.store = ""
+	
+	CatalogProductCustomOptionListResponseParam catalogProductCustomOptionListResponseParam = mageProxy.getMageApiModelServerWsiHandlerPort().catalogProductCustomOptionList(cpcolp)
+	
+	CatalogProductCustomOptionListArray catalogProductCustomOptionListArray = catalogProductCustomOptionListResponseParam.result
+	
+	catalogProductCustomOptionListArray.complexObjectArray.each {
+	  def productCustomOptionListExpando = new Expando()
+	  productCustomOptionListExpando.optionId = it.optionId
+	  productCustomOptionListExpando.title = it.title
+	  productCustomOptionListExpando.type = it.type
+	  productCustomOptionListExpando.sortOrder = it.sortOrder
+	  
+	  customProductOptionList.add(productCustomOptionListExpando)
+	}
+	customProductOptionList
+  }
+  
+  /**
+   * Get a list of product options values based on sessionId
+   *
+   * @param sessionId
+   * @param productId
+   * @return List of Product Custom Option Value (expando object containing the various attributes)
+   */
+  List getProductCustomOptionValue(String sessionId, String productId) {
+	  
+	def customProductOptionList = []
+  
+	CatalogProductCustomOptionListRequestParam cpcolp = new CatalogProductCustomOptionListRequestParam()
+	cpcolp.sessionId = sessionId
+	cpcolp.productId = productId
+	cpcolp.store = ""
+	
+	CatalogProductCustomOptionListResponseParam catalogProductCustomOptionListResponseParam = mageProxy.getMageApiModelServerWsiHandlerPort().catalogProductCustomOptionList(cpcolp)
+	
+	CatalogProductCustomOptionListArray catalogProductCustomOptionListArray = catalogProductCustomOptionListResponseParam.result
+	
+	catalogProductCustomOptionListArray.complexObjectArray.each {
+	  def productCustomOptionListExpando = new Expando()
+	  productCustomOptionListExpando.optionId = it.optionId
+	  productCustomOptionListExpando.title = it.title
+	  productCustomOptionListExpando.type = it.type
+	  productCustomOptionListExpando.sortOrder = it.sortOrder
+	  
+	  customProductOptionList.add(productCustomOptionListExpando)
+	}
+	customProductOptionList
+  }
 }
